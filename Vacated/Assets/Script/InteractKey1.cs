10bc215040bba2e5268b9692189b54cbf0fc9788
AidanMcNa <aidanfmcnamara@gmail.com>
2023-06-27 14:48:58 -0500

    Interaction script

---
+++ Vacated/Assets/Script/InteractKey1.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractKey1 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (gameObject.tag == "Bedroom1Key")
+        {
+            // destroy this object
+            Destroy(gameObject);
+        }
+    }
+    
+
+}
