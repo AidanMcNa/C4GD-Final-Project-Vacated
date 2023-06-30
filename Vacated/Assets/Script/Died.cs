bf16294bb168310c2d8836a91b03997bdfc9297d
AidanMcNa <aidanfmcnamara@gmail.com>
2023-06-30 10:11:30 -0500

    Finished game

--- Vacated/Assets/Script/Died.cs
+++ Vacated/Assets/Script/Died.cs
@@ -5,33 +5,32 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class End : MonoBehaviour
+public class Died : MonoBehaviour
 {
-    public GameObject endScreen;
-    public GameObject endScreen2;
-    public GameObject objectives;
     public GameObject restart;
+    public GameObject deadScreen1;
+    public GameObject deadScreen2;
+    public GameObject objectives;
     // Start is called before the first frame update
     void Start()
     {
-        endScreen.SetActive(false);
-        endScreen2.SetActive(false);
+        deadScreen1.SetActive(false);
+        deadScreen2.SetActive(false);
         restart.SetActive(false);
-       
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     void OnTriggerEnter(Collider collider)
     {
-        if (gameObject.tag == "ExitBox")
+        if (gameObject.tag == "MonsterKill")
         {
             // destroy this object
-            endScreen.SetActive(true);
-            endScreen2.SetActive(true);
+            deadScreen1.SetActive(true);
+            deadScreen2.SetActive(true);
             objectives.SetActive(false);
             restart.SetActive(true);
             Destroy(gameObject);
@@ -39,5 +38,4 @@ public class End : MonoBehaviour
 
         }
     }
-    
 }
